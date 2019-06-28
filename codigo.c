124161de5fcc67190d85f415003abac43dd5e9da
abelbague <a.bague@alumnos.upm.es>
2019-06-28 17:08:37 +0200

    inicio de programa

--- codigo.c
+++ codigo.c
@@ -1,28 +1,72 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#define NUM_EJ 5
+#define NUM_SETS 3
+#define REST_TIME 60
 
 typedef struct{
-	int reps, tiempo;
+	int reps;
 }series;
 
 typedef struct{
 	char nombre[20];
-	series sets[20];
+	series sets[NUM_SETS];
 }ejercicio;
 
 typedef struct{
 	char nombre[20];
-	ejercicio ej[20];
+	ejercicio ej[NUM_EJ];
 }rutina;
 
 int main(){
-	int i=0, j=0, k=0, l=0, op, op2, op3, op4, num_ej, num_sets, num_reps, rest;
+	int i=0, j=0, k=0, l=0, nuevo = 1, guia, op, op2, op3, op4, num_ej, num_sets, num_reps, rest;
+	char nombre[20], file_name[20];
 	float peso, IMC, altura;
+	FILE *r, *ej, *rep, *us;
 	rutina rut[2];
 	
-	printf("\n ~ Bienvenido a SmartProgress ~\n ________________________________");
-	printf("\n\n\n Escoge una opcion:");
+	r = fopen("Files/Rutinas.txt", "r");
+	ej = fopen("Files/Ejercicios", "r");
+	rep = fopen("Files/Repeticiones", "r");
+	us = fopen("Files/Usuarios", "r");
+	
+	printf("\n\t\t\t\t\t  ~  Bienvenido a SmartProgress  ~ \n\t\t\t\t\t __________________________________");
+	printf("\n\n\n > Dinos tu nombre para personalizar la experiencia: ");
+		scanf("%s", nombre[20]);
+		_strupr(nombre);
+	while(!feof(us)){
+		fscanf(us, "%[^\n]\n", &file_name);
+		if(strcmp(file_name,nombre)==0){
+			//system("cls");
+			printf("\n Hola %s, ya has usado este programa anteriormente asi que suponemos que sabes como se usa.", nombre);
+			nuevo = 0;
+			break;
+		}
+	}
+	if(nuevo == 1){
+		system("cls");
+		printf("\nEs la primera vez que utilizas este programa. Quieres leer una guia rapida de uso?" );
+		printf("\n\n [1] Si\n [2] No ");
+			scanf("%i", &guia);
+		while(guia != 1 && guia != 2){
+			system("cls");
+			printf("\n Opcion incorrecta.\n Quieres leer una guia rapida de uso?");
+			printf("\n\n [1] Si\n [2] No ");
+				scanf("%i", &guia);
+		}
+		fclose(us);
+		
+		if(nuevo==0){
+        	fopen("Ficheros/Usuarios.txt", "a");
+        	fprintf(us,"\n%s",nombre);
+    	}
+    	
+		if(guia == 1){
+			printf("El objetivo de SmartProgress es ayudarte a que logres tus retos fisicos de manera rapida usando el registro de progresiones implementado en las rutinas.");
+		}
+	}
+	printf("\n\n\n > En que podemos ayudarte?: ");
 	printf("\n\n\n\t [1] Consultar mi estado fisico\n\n\t [2] Entrenamientos y rutinas ");
 		scanf("%i", &op);
 	
@@ -66,20 +110,17 @@ int main(){
 				
 					for (i = 0; i < num_ej; i++){
 						system("cls");
-						printf("\n\n\n Ejercicio numero %i: ", i+1);
+						printf("\n\n Ejercicio numero %i: ", i+1);
 						printf("\n\n Introduce el nombre del ejercicio: \n\t");
 							scanf("%s", &rut[0].ej[i].nombre);
 						printf("\n\n Introduce el numero de series: \n\t");
 							scanf("%i", &num_sets);
 						printf("\n\n Numero de repeticiones por serie: \n\t");
 							scanf("%i", &num_reps);
-						printf("\n\n Tiempo de descanso (en segundos): \n\t"); //hacer sistema de conversion de tiempo
-							scanf("%i", &rest);
+				
 							
-						for (l = 0; l < num_sets; l++){
+						for (l = 0; l < num_sets; l++)
 							rut[0].ej[i].sets[l].reps = num_reps;
-							rut[0].ej[i].sets[l].tiempo = rest;
-						}
 					}
 					system("cls");
 					printf("\n\n Pulse cualquier tecla para comenzar el entrenamiento...");
@@ -93,13 +134,13 @@ int main(){
 							system("cls");
 							printf("\n\nSerie %i", k+1);
 							printf("\n\n\n\t Tienes que hacer %i %s", rut[0].ej[i].sets[k].reps, rut[0].ej[i].nombre);
-							printf("\n\n\n Pulsa cualquier tecla cuando termines la serie...");
+							printf("\n\n\n\n Pulsa cualquier tecla cuando termines la serie...");
 							getch();
 							system("cls");
-							printf("\n Descansa durante %i''", rut[0].ej[i].sets[k].tiempo);
+							printf("\n Descansa durante %i''", REST_TIME);
 							printf("\n\n Cuantas repeticiones has podido hacer?\n\t");
 								scanf("%i", &rut[1].ej[i].sets[k].reps);
-							printf("\n\n\n Toca cualquier tecla cuando estes listo para continuar...");
+							printf("\n\n\n\n Toca cualquier tecla cuando estes listo para continuar...");
 							getch();
 						}
 					}
