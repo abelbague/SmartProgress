92e8233505e7cc01abcc3ca5f0665451e1bb0756
abelbague <a.bague@alumnos.upm.es>
2019-06-29 23:02:46 +0200

    detector de rutinas

--- codigo.c
+++ codigo.c
@@ -18,54 +18,52 @@ typedef struct{
 typedef struct{
 	char nombre[20];
 	ejercicio ej[NUM_EJ];
-}rutina;
+}rutinas;
 
 int main(){
-	int i, j, k, l, m, n, nuevo = 1, reps, guia, op, op2, op3, op4, aux, aux2 = 0, repes[50], num_ej, num_sets, num_reps, rest;
-	char nombre[20], file_name[20], rutina[20];
+	int i, j, k, l=0, m, n, nuevo = 1, guia, op, op2, op3, op4, aux, aux2 = 0, out = 0, num_reps, num_ej, rest, repes[50], rutina_len;
+	char name[20], file_name[20], rutina[20];
 	float peso, IMC, altura;
 	FILE *ru, *ej, *rep, *us;
-	rutina rut[NUM_RUT];
+	rutinas rut[NUM_RUT];
 	
-	ru = fopen("Files/Rutinas.txt", "r");
+	ru = fopen("Files/Rutinas.txt", "a+");
 	ej = fopen("Files/Ejercicios.txt", "r");
 	rep = fopen("Files/Repeticiones.txt", "r");
-	us = fopen("Files/Usuarios.txt", "r");
+	us = fopen("Files/Usuarios.txt", "a+");
 	
 	for(i=0;i<NUM_RUT;i++) // Se almacenan las rutinas en estructuras para luego ser comparadas
 	{
-		fscanf(ru, "[^\n]\n", &rut[i].nombre);
+		fscanf(ru, "%s\n", rut[i].nombre);
 		for(j=0;j<NUM_EJ;j++)
 		{
-			fscanf(ej, "%[^\n]\n", &rut[i].ej[j].nombre);
-			fscanf(rep, "%i\n", &reps);
+			fscanf(ej, "%s\n", rut[i].ej[j].nombre);
+			fscanf(rep, "%i\n", &num_reps);
 			
 			for(k=0;k<NUM_SETS;k++)
-				rut[i].ej[j].sets[k].reps = reps; // mismo numero de repticiones para todas las series de un mismo ejercicio
+				rut[i].ej[j].sets[k].reps = num_reps; // mismo numero de repticiones para todas las series de un mismo ejercicio
 		}
 	}
+	printf("Nombre de la rutina %s", rut[3].nombre);
 	// printf("sets %i", rut[3].ej[2].sets[2].reps); para ver si almacena correctamente los datos
 	
 	printf("\n\t\t\t\t\t  ~  Bienvenido a SmartProgress  ~ \n\t\t\t\t\t __________________________________");
 	printf("\n\n\n > Teclea tu nombre para personalizar la experiencia: ");
-		gets(nombre);
-		_strupr(nombre);
+		gets(name);
+		_strupr(name);
 		
 	while(!feof(us)){
-		fscanf(us, "%[^\n]\n", &file_name);
-		if(strcmp(file_name,nombre)==0){
+		fscanf(us, "%s\n", &file_name);
+		if(strcmp(file_name,name)==0){
 			system("cls");
-			printf("\n Hola %s, ya has usado este programa anteriormente asi que suponemos que sabes como se usa.", nombre);
+			printf("\n Hola %s, ya has usado este programa anteriormente asi que suponemos que sabes como se usa.", name);
 			nuevo = 0;
 			break;
 		}
 	}
 	system("cls");
 	if(nuevo == 1){
-		
-		rewind(us);
-		fopen("Ficheros/Usuarios.txt","a"); // NO ESCRIBE NUEVOS USUARIOS
-        fprintf(us,"\n%s", nombre);
+        fprintf(us,"\n%s", name);
         
 		printf("\nEs la primera vez que utilizas este programa. Quieres leer una guia rapida de uso?" );
 		printf("\n\n\n [1] Si\n\n [2] No ");
@@ -82,7 +80,7 @@ int main(){
 			printf("El objetivo de SmartProgress es ayudarte a que logres tus retos fisicos de manera rapida usando nuestro registro de progresiones implementado en las rutinas.");
 		}
 	}
-	printf("\n\n\n > En que podemos ayudarte?: ");
+	printf("\n\n\n > En que podemos ayudarte %s?: ", name);
 	printf("\n\n\n\t [1] Consultar mi estado fisico\n\n\t [2] Entrenamientos y rutinas \n\n\t [3] Salir");
 		scanf("%i", &op);
 	
@@ -99,7 +97,7 @@ int main(){
 						scanf("%f", &peso);
 					printf("\n Introduce tu altura en metros (por ejemplo 1.76): \n\t");
 						scanf("%f", &altura);
-					IMC = peso / (altura*altura);
+					IMC = (peso / (altura*altura))*100;
 					printf("\n\t Su IMC es de %g \n\n", IMC);
 				break;
 				
@@ -121,28 +119,37 @@ int main(){
 			system("cls");
 				
 			switch(op3){
-				case 1:	
+				case 1:
 					printf("\n\tQue grupo muscular quieres entrenar? ");
-						gets(rutina);
-						system("cls");
+					scanf("%s", rutina);
+					rutina_len = strlen(rutina);
+					//system("cls");
 						
 					for(l=0; l<NUM_RUT; l++){
-						if(strcmp(rut[i].nombre,rutina)==0){
-							aux = i; // referencia de la rutina seleccionada
+						if(strncmp(rut[l].nombre, rutina, rutina_len)==0){ // compara con tantos caracteres como se introduzcan
+							aux = l; // referencia de la rutina seleccionada
+							out = 1;
+							system("cls");
+							printf("\n\tCuanto tiempo de descanso quieres tener entre series?");
+								fflush(stdin);
+								scanf("%i", &rest);
+							printf("\n\n Pulsa cualquier tecla para empezar el entrenamiento ");
+								fflush(stdin);
+								getch();
 							printf("\n\t Rutina de %s", rut[aux].nombre);
 							
 							for (m = 0; m < NUM_EJ; m++){
-								
 								printf("\n\n > Ejercicio %i\t%s", i+1, rut[aux].ej[m].nombre);
+								
 								for (n = 0; n < NUM_SETS; n++){
 									
-									printf("\n\n Serie %i", n+1);
+									printf("\n\n Serie %i/%i", n+1, NUM_SETS);
 									printf("\tTienes que hacer %i %s", rut[aux].ej[m].sets[n].reps, rut[aux].ej[m].nombre);
 									printf("\n\n\n\n\n > Pulsa cualquier tecla cuando termines la serie ");
 										getch();
 									system("cls");
 									
-									printf("\n Descansa durante %i''", REST_TIME);
+									printf("\n Descansa durante %i''", rest);
 									printf("\n\n Cuantas repeticiones has podido hacer?\n\t");
 										scanf("%i", &repes[aux2]);
 											aux2++;
@@ -151,13 +158,13 @@ int main(){
 									system("cls");
 								}
 							}
-							printf("\n > Enhorabuena has logrado acabar el entrenamiento");
-								
-						}
-						else{
-							printf("No hay ninguna rutina con ese nombre. Quieres crearla?");
-						}
+							printf("\n\t > Bien hecho! Rutina de %s terminada", rut[aux].nombre);
+							printf("\n Estos son tus resultados:");	
+							break;	
+						}	
 					}
+					if(out == 0)
+						printf("No hay ninguna rutina con ese nombre. Quieres crearla?");
 					break;
 					
 				case 2:
@@ -172,13 +179,13 @@ int main(){
 						printf("\n\n Ejercicio numero %i: ", i+1);
 						printf("\n\n Introduce el nombre del ejercicio: \n\t");
 							scanf("%s", &rut[0].ej[i].nombre);
-						printf("\n\n Introduce el numero de series: \n\t");
-							scanf("%i", &num_sets);
-						printf("\n\n Numero de repeticiones por serie: \n\t");
-							scanf("%i", &num_reps);
+						//printf("\n\n Introduce el numero de series: \n\t");
+							//scanf("%i", &num_sets);
+						//printf("\n\n Numero de repeticiones por serie: \n\t");
+							//scanf("%i", &num_reps);
 				
 							
-						for (l = 0; l < num_sets; l++)
+						for (l = 0; l < NUM_SETS; l++)
 							rut[0].ej[i].sets[l].reps = num_reps;
 					}
 					system("cls");
@@ -186,10 +193,10 @@ int main(){
 					getch();
 					
 					printf("\n\t Rutina de %s", rut[0].nombre);
-					for (i = 0; i < num_ej; i++){
+					for (i = 0; i < NUM_EJ; i++){
 						system("cls");
 						printf("\n Ejercicio %i\t%s", i+1, rut[0].ej[k].nombre);
-						for (k = 0; k < num_sets; k++){
+						for (k = 0; k < NUM_SETS; k++){
 							system("cls");
 							printf("\n\nSerie %i", k+1);
 							printf("\n\n\n\t Tienes que hacer %i %s", rut[0].ej[i].sets[k].reps, rut[0].ej[i].nombre);
@@ -216,7 +223,7 @@ int main(){
 		break;
 		
 		case 3:
-			printf("\n Hasta la proxima %s", nombre);
+			printf("\n Hasta la proxima %s", name);
 			break;
 			
 		default:
